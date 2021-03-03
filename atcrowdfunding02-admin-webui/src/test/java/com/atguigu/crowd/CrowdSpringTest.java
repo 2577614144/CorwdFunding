8e7f2e7f2948e3c42472113a53503cb7b1ee6e79
lipine <2577614144@qq.com>
2021-03-03 16:30:38 +0800

    spring 整合 mybatis

---
+++ atcrowdfunding02-admin-webui/src/test/java/com/atguigu/crowd/CrowdSpringTest.java
@@ -0,0 +1,45 @@
+package com.atguigu.crowd;
+
+import com.atguigu.crowd.entity.Admin;
+import com.atguigu.crowd.mapper.AdminMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @Author: lipine
+ * @Date: 2021/3/3 14:41
+ * @Description: 创建 Spring 的 Junit 测试类
+ * @Version 1.0
+ */
+
+// 指定 Spring 给 Junit 提供的运行器类
+@RunWith(SpringJUnit4ClassRunner.class)
+// 加载 Spring 配置文件的注解
+@ContextConfiguration(locations = {"classpath:spring-persist-mybatis.xml"})
+public class CrowdSpringTest {
+
+    @Autowired
+    private DataSource dataSource;
+    @Test
+    public void testDataSource() throws SQLException {
+    // 1.通过数据源对象获取数据源连接
+        Connection connection = dataSource.getConnection();
+        // 2.打印数据库连接
+        System.out.println(connection);
+    }
+
+    @Autowired
+    private AdminMapper adminMapper;
+    @Test
+    public void testAdminMapperAutowired() {
+        Admin admin = adminMapper.selectByPrimaryKey(1);
+        System.out.println(admin);
+    }
+}
